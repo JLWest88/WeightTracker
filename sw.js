90f2a2894cb2f660aecf3799f37ddf12b17958d3
JLWest88 <jlwest88@hotmail.com>
2026-02-02 01:39:00 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wt-cache-v5";
+const CACHE_NAME = "wt-cache-v6";
 const ASSETS = [
   "./",
   "./index.html",
@@ -51,6 +51,7 @@ self.addEventListener("fetch", (event) => {
     caches.match(req).then((cached) => cached || fetch(req))
   );
 });
+
 
 
 
